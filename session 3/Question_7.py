d4f18803e20cd838de08bd819f51f0d45795e9b9
parsa <parsabineshpazhooh@gmail.com>
2026-08-04 19:49:10 +0330

    Add session 02 exercises

--- session 3/Question_7.py
+++ session 3/Question_7.py
@@ -1,6 +1,7 @@
 color_1 = input("Enter first color: ")
 color_2 = input("Enter second color: ")
 color_3 = input("Enter third color: ")
+color_1, color_2, color_3 = color_1.lower(), color_2.lower(), color_3.lower()
 if color_3 == color_1 == color_2:
     print("all colors are same")
 
--- session 3/Question_8.py
+++ session 3/Question_8.py
@@ -1,9 +1,9 @@
-AllOfMoney = int(input("enter any number: "))
+all_of_money = int(input("enter any number: "))
 Deposit = int(input("Enter Money you wanna deposit:"))
 if Deposit > 0:
-    if Deposit > AllOfMoney:
+    if Deposit > all_of_money:
         print("Deposit is greater than all of your money\nso you can't deposit.")
-    if Deposit < AllOfMoney:
-        print("Deposited", AllOfMoney - Deposit)
+    if Deposit < all_of_money:
+        print("Deposited", all_of_money - Deposit)
 else:
     print("You can't Deposit a number lower than Zero")
